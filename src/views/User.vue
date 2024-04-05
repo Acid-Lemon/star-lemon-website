caa8e7a3a5c7c8d1ca851dea39f026cceeae826b
Acid-Lemon <3573045100@qq.com>
2024-04-05 15:09:59 +0800

    hide navigation_bar

--- src/views/User.vue
+++ src/views/User.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-	
+	<div class="h-full w-full">
+    <div class="bg-[url('/static/background/12.jpg')] bg-cover h-[40%]"></div>
+  </div>
 </template>
 <style scoped>
 </style>

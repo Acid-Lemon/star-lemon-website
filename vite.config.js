77f6c52fc5e75a0f5595040e99465de4a2c30876
star <1598624985@qq.com>
2024-04-09 19:30:08 +0800

    add element-plus auto import support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import {
-	defineConfig
+    defineConfig
 } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
+
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
 
 /** ==== 处理 tailwind cli 的自动启动和打包 ==== */
 const child_process = require('child_process')
@@ -10,28 +14,35 @@ let tailwindMode = process.env.NODE_ENV
 // 主进程输出
 console.log(`[tailwindcss] 开始${tailwindMode === 'production' ? '生产环境打包' : '开发模式监听'}`);
 child_process.execSync('npm run tailwind-build', {
-	cwd: __dirname
+    cwd: __dirname
 });
 if (tailwindMode !== 'development') {
-	child_process.exec(
-		'npm run tailwind-dev', {
-			cwd: __dirname, // 切换目录到当前项目，必须
-		},
-		(error, stdout, stderr) => {
-			// tailwind --watch 是一个持久进程，不会立即执行回调
-			// process.stdout.write('tailwind success')
-			if (error) {
-				console.error('[tailwindcss] 异常，请检查');
-				console.error(error);
-				console.error(stdout);
-				console.error(stderr);
-			}
-			if (tailwindMode === 'production') {
-				console.log('[tailwindcss] 生产环境打包完成');
-			}
-		})
+    child_process.exec(
+        'npm run tailwind-dev', {
+            cwd: __dirname, // 切换目录到当前项目，必须
+        },
+        (error, stdout, stderr) => {
+            // tailwind --watch 是一个持久进程，不会立即执行回调
+            // process.stdout.write('tailwind success')
+            if (error) {
+                console.error('[tailwindcss] 异常，请检查');
+                console.error(error);
+                console.error(stdout);
+                console.error(stderr);
+            }
+            if (tailwindMode === 'production') {
+                console.log('[tailwindcss] 生产环境打包完成');
+            }
+        })
 }
 
 export default defineConfig({
-	plugins: [uni()]
+    plugins: [
+        uni(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        })]
 });

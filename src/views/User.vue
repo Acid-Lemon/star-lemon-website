53329da1d8156f060cf7bc0008c753c0572cfba2
Acid-Lemon <3573045100@qq.com>
2024-04-13 17:29:58 +0800

    update(user)

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,22 +1,28 @@
 <script>
+  import {
+    ElMessageBox
+  } from "element-plus";
+
   export default {
     data() {
       return {
         dialogVisible: false,
         date: null,
-        username:null,
-        motto:null
-        
+        username: null,
+        motto: null
+
       };
     },
     methods: {
       handleClose(done) {
-        this.$confirm('确认关闭？(未提交内容不会保存)')
-          .then(() => {
+        ElMessageBox.confirm('确认关闭？（未提交的信息不会保存）', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }).then(() => {
             done();
           })
-          .catch(() => {
-          });
+          .catch(() => {});
       }
     },
   }
@@ -36,36 +42,29 @@
         <p class="font-['SYST'] text-[14px] mr-[20px] leading-none pb-[5px]">生日：2006年3月1日</p>
         <p class="font-['SYST'] text-[14px] opacity-50 leading-none pb-[5px]">Hi！希望你开心～</p>
       </div>
-      <el-button
-        class="absolute bottom-[-5vh] right-[10vw]" plain @click="dialogVisible = true">
+      <el-button class="absolute bottom-[-5vh] right-[10vw]" plain @click="dialogVisible = true">
         编辑信息
       </el-button>
-      <el-dialog title="编辑个人信息" v-model="dialogVisible" width="30%" center :before-close="handleClose">
-        <div class="flex flex-col justify-between items-start h-[180px] w-[80%]">
-        <div class="w-full">
-          <span>用户名：</span>
-          <el-input v-model="username" class="w-full" />
-        </div>
-        <div class="w-full">
-        <span>生日：</span>
-        <div class="w-full">
-        <el-date-picker
-          v-model="date"
-          type="date"
-          size="default"
-        />
-        </div>
-        </div>
-        <div class="w-full">
-        <span>个性签名：</span>
-        <el-input v-model="motto" class="w-full" />
-        </div>
-        </div>
+      <el-dialog title="编辑个人信息" v-model="dialogVisible" center :before-close="handleClose" class="flex flex-col items-center justify-center h-[320px]" width="30%">
+          <div class="w-full my-[5px]">
+            <span>用户名：</span>
+            <el-input v-model="username" class="w-full" />
+          </div>
+          <div class="w-full my-[5px]">
+            <span>生日：</span>
+            <div class="w-full my-[5px]">
+              <el-date-picker v-model="date" type="date" size="default" style="width: 100%" />
+            </div>
+          </div>
+          <div class="w-full my-[5px]">
+            <span>个性签名：</span>
+            <el-input v-model="motto" class="w-full" />
+          </div>
         <template #footer>
-        <div class="dialog-footer flex flex-row justify-around">
-          <el-button type="primary" @click="dialogVisible = false">确定</el-button>
-          <el-button @click="dialogVisible = false">取消</el-button>
-        </div>
+          <div class="dialog-footer">
+            <el-button @click="dialogVisible = false" class="mx-[40px]">取消</el-button>
+            <el-button type="primary" @click="dialogVisible = false"  class="mx-[40px]">确定</el-button>
+          </div>
         </template>
       </el-dialog>
     </div>

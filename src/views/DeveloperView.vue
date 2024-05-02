d931d7abdaa5d6287185d1b98d311470cf5abadc
Acid-Lemon <3573045100@qq.com>
2024-05-02 23:54:18 +0800

    update(developerView)

--- src/views/DeveloperView.vue
+++ src/views/DeveloperView.vue
@@ -21,7 +21,13 @@ export default {
   <div class="w-full h-full flex flex-col px-[20px]">
     <div class="w-full h-[20%]">
     </div>
-    <div class="w-full h-[40%] flex flex-row items-center content-center">
+    <div class="w-full h-[20%]">
+      <div class="text-[30px] font-['SYST']">
+        此网站所用到的技术
+      </div>
+      <img alt="Vue" class="w-[10vh] h-[10vh]" src="/static/svg/Vue.svg"/>
+    </div>
+    <div class="w-full h-[30%] flex flex-row items-center content-center">
       <div class="w-[20%] h-full flex flex-col justify-center items-center">
         <img alt="头像"
              class="w-[15vh] h-[15vh] rounded-full hover:rotate-[360deg] duration-700 border border-[#dedede] m-[20px]"
@@ -35,14 +41,15 @@ export default {
         </div>
       </div>
       <div class="w-[80%] h-full p-[20px] ">
-        <div class="w-full h-full bg-[#dedede] rounded-[25px] p-[20px] text-[40px] font-['SJJS']">
-          star：诶嘿～
+        <div class="w-full h-full bg-[#dedede] rounded-[25px] p-[20px] text-[20px] font-['SJJS']">
+          诶嘿～
         </div>
       </div>
     </div>
-    <div class="w-full h-[40%] flex flex-row items-center content-center">
+    <div class="w-full h-[30%] flex flex-row items-center content-center">
       <div class="w-[80%] h-full p-[20px]">
-        <div class="w-full h-full bg-[#dedede] rounded-[25px]">
+        <div class="w-full h-full bg-[#dedede] rounded-[25px] p-[20px] text-[20px] font-['SJJS']">
+          嗯...不要在该努力的日子选择安逸<br/>嗯，就这样
         </div>
       </div>
       <div class="w-[20%] h-full flex flex-col justify-center items-center">

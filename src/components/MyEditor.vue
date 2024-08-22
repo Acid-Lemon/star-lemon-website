af5db84b3883e4e126a44c9af12ccc00a6c44f08
Acid-Lemon <3573045100@qq.com>
2024-08-22 23:11:05 +0800

    add vditor & delete wangeditor

--- src/components/MyEditor.vue
+++ src/components/MyEditor.vue
@@ -1,66 +1,42 @@
 <script>
+import Vditor from 'vditor';
 
-import { onBeforeUnmount, ref, shallowRef } from 'vue'
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
-
-export default{
-  components: { Editor, Toolbar },
-  emits: ['update:modelValue'],
-  watch:{
-    valueHtml:{
-      handler(newVal,oldVal){
-        this.$emit('update:modelValue',newVal)
-      }
-    }
-  },
-  setup() {
-    // 编辑器实例，必须用 shallowRef
-    const editorRef = shallowRef()
-
-    // 内容 HTML
-    const valueHtml = ref('')
-
-    const toolbarConfig = {}
-    const editorConfig = { placeholder: '请输入内容...' }
-
-    // 组件销毁时，也及时销毁编辑器
-    onBeforeUnmount(() => {
-      const editor = editorRef.value
-      if (editor == null) return
-      editor.destroy()
-    })
-
-    const handleCreated = (editor) => {
-      editorRef.value = editor // 记录 editor 实例，重要！
-    }
-
-    return {
-      editorRef,
-      valueHtml,
-      mode: 'default', // 或 'simple'
-      toolbarConfig,
-      editorConfig,
-      handleCreated
-    };
-  }
-}
+export default {
+    name: 'MyEditor',
+    data() {
+        return {
+            vditorInstance: null
+        };
+    },
+    emits: ['update:value'],
+    mounted() {
+        this.vditorInstance = new Vditor(this.$refs.vditor, {
+            // 配置 Vditor 选项
+            cache: false,
+            lang: 'zh_CN',
+            width: '100%',
+            height: '100%',
+            mode: 'ir',
+            input: (value) => {
+                // 监听输入事件
+                this.$emit('update:value', value);
+            },
+        });
+    },
+    beforeDestroy() {
+        if (this.vditorInstance) {
+            this.vditorInstance.destroy();
+        }
+    },
+};
 </script>
 
 <template>
-  <link rel="stylesheet" href="/static/css/wangeditor-style.css" type="text/css" />
-    <div style="border: 1px solid #ccc">
-    <Toolbar
-        style="border-bottom: 1px solid #ccc"
-        :editor="editorRef"
-        :defaultConfig="toolbarConfig"
-        :mode="mode"
-    />
-    <Editor
-        style="height: calc(100vh - 140px); overflow-y: hidden;"
-        v-model="valueHtml"
-        :defaultConfig="editorConfig"
-        :mode="mode"
-        @onCreated="handleCreated"
-    />
+    <div class="w-full h-full">
+        <link href="/static/css/vditor.css" rel="stylesheet"/>
+        <div ref="vditor"></div>
     </div>
 </template>
+
+<style scoped>
+</style>

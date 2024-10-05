c1e7b9219bf2850caff2a5a2940477b3e1ed2738
Acid-Lemon <3573045100@qq.com>
2024-10-05 23:01:47 +0800

    optimization(ReadView)

--- src/views/write/ReadView.vue
+++ src/views/write/ReadView.vue
@@ -107,7 +107,10 @@ export default {
                 <div class="w-full min-h-[60vh] flex flex-row items-start justify-center bg-[#F8FAFD]">
                     <div class="w-[15vw] flex flex-col items-center justify-start"></div>
                     <div class="w-[70vw] flex flex-col items-center justify-start">
-                        <md-parser :content=article.content></md-parser>
+                        <div class="my-[20px] bg-white shadow-md p-[20px]">
+                            <md-parser :content=article.content></md-parser>
+                        </div>
+
                     </div>
                     <div class="w-[15vw] flex flex-col items-start justify-start">
                         <el-affix :offset="5">

91efbba3afee3f6586aede4e0702db98ed1771f4
Acid-Lemon <3573045100@qq.com>
2025-01-10 17:23:48 +0800

    simple fix

--- router.js
+++ router.js
@@ -234,18 +234,6 @@ const router = createRouter({
             component: () => import("@/src/views/useful_tools/separate-audio.vue")
         },
         {
-            path: "/particles/particle_text",
-            name: "particle_text",
-            meta: {
-                title: "粒子文字",
-                show: {
-                    navigation_bar: false,
-                    side_navigation_bar: false
-                }
-            },
-            component: () => import("@/src/views/particles/particle-text.vue")
-        },
-        {
             path: "/404",
             name: "404",
             meta: {

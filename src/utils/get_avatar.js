206e1396d2315e06de9f7c52ec80cc71c11212ac
star <1598624985@qq.com>
2025-01-31 22:06:06 +0800

    update

--- src/utils/get_avatar.js
+++ src/utils/get_avatar.js
@@ -1,44 +1,54 @@
-import {call_api} from "@/src/utils/cloud";
-import {ElNotification} from "element-plus";
 import axios from "axios";
+import {ElNotification} from "element-plus";
+
+import {call_api} from "@/src/utils/cloud";
 import {use_user_info_store} from "@/src/stores/userInfo";
 
+const user_info_store = use_user_info_store();
+
 async function get_avatar() {
-    let avatar_url = null;
-
-    const user_info_store = use_user_info_store();
-    const user_info = user_info_store.user_info;
-    let avatar_name = user_info?.avatar?.name;
-    let type = user_info?.avatar?.type;
-    if (avatar_name) {
-        if (type === "upload") {
-            let avatar_url_res = await call_api("user/profile/get_upload_avatar_temp_url", {
-                image_name: avatar_name,
-            });
-
-            if (!avatar_url_res.success) {
-                ElNotification({
-                    title: 'Error',
-                    message: avatar_url_res,
-                    type: 'error',
-                });
-                console.log(avatar_url_res);
-                return;
-            } else {
-                const response = await axios.get(avatar_url_res.data.temp_url, {
-                    responseType: 'blob',
-                    headers: {
-                        'Cache-Control': 'max-age=86400', // 缓存24小时
-                    },
-                });
-                avatar_url = URL.createObjectURL(response.data);
-            }
-        } else {
-            avatar_url = "/static/avatar/" + avatar_name
-        }
+    let user_info = user_info_store.user_info;
+    if (!user_info.hasOwnProperty("avatar")) {
+        return null;
     }
 
-    return avatar_url
+    let type = user_info.avatar.type;
+
+    // 直接有第三方头像链接
+    if (type === "url") {
+        return user_info.avatar.url;
+    }
+
+    // 本地头像
+    if (type === "local") {
+        return "/static/avatar/" + user_info.avatar.name;
+    }
+
+    // 头像是用户自己上传的，需要获取临时访问链接
+    let avatar_url_res = await call_api("user/profile/get_upload_avatar_temp_url", {
+        image_name: user_info.avatar.name
+    });
+
+    if (!avatar_url_res.success) {
+        ElNotification({
+            title: 'Error',
+            message: avatar_url_res,
+            type: 'error',
+        });
+        console.error(avatar_url_res);
+        return null;
+    }
+
+    // 通过临时链接获取图片内容
+    return await axios.get(avatar_url_res.data.temp_url, {
+        responseType: 'blob',
+        headers: {
+            'Cache-Control': 'max-age=86400' // 缓存24小时
+        }
+    }).then(({data}) => {
+        // 获取到了图片内容，生成本地临时资源访问链接
+        return URL.createObjectURL(data);
+    });
 }
 
 export {
--- src/utils/truncate_text.js
+++ src/utils/truncate_text.js
@@ -1,24 +1,20 @@
-function truncate_text(text, max_chinese_length) {
-    let currentLength = 0;
-    let current_text = '';
+function truncate_text(text, max_chinese_len) {
+    // 分隔标准上两个英文字符与一个中文字符等价
+    const max_english_len = max_chinese_len * 2;
 
-    for (let char of text) {
-        // 检查字符是否为中文字符（通过字符的Unicode范围）
-        if (char.match(/[\u4e00-\u9fff]/)) {
-            currentLength += 2; // 中文字符长度加2
-        } else {
-            currentLength += 1; // 英文字符长度加1
+    let text_len = text.length, english_len_sum = 0;
+    for (let i = 0; i < text_len; i++) {
+        // 是否是中文字符（通过字符的Unicode范围），1个英文字符对应1个english_len，1中文字符2个english_len
+        let ti_english_len = text[i].match(/[\u4e00-\u9fff]/) ? 2 : 1;
+
+        if (english_len_sum + ti_english_len > max_english_len) {
+            return text.substring(0, i);
         }
 
-        if (currentLength <= max_chinese_length * 2) {
-            current_text += char;
-        } else {
-            current_text += '...';
-            break;
-        }
+        english_len_sum += ti_english_len;
     }
 
-    return current_text;
+    return text;
 }
 
 export {

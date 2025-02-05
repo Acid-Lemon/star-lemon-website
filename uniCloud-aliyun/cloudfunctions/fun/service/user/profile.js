3c67c88e8fab1577132cced5e65def267fd77761
Acid-Lemon <3573045100@qq.com>
2025-02-05 13:36:58 +0800

    Enhance error handling and optimize user profile updates

--- uniCloud-aliyun/cloudfunctions/fun/service/user/profile.js
+++ uniCloud-aliyun/cloudfunctions/fun/service/user/profile.js
@@ -44,25 +44,23 @@ module.exports = class Service_User_Info extends Service {
         let upload_options = await this.service.cloud_storage.profile.get_avatar_upload_options(image_name);
 
         let user_avatar = this.ctx.user.avatar;
-        // 如果和目前user的avatar不一致，则进行更新
-        if (!user_avatar || (user_avatar.type !== "upload" || user_avatar.name !== image_name)) {
-            if (user_avatar) {
-                let diff = Date.now() - user_avatar.update_at;
-                if (diff < 24 * 60 * 60 * 1000) {
-                    this.throw(errors.codes.rate_limit, "距离上次更改头像不足一天");
-                }
 
-                await this.service.cloud_storage.profile.delete_avatar(user_avatar.name);
+        if (user_avatar) {
+            let diff = Date.now() - user_avatar.update_at;
+            if (diff < 24 * 60 * 60 * 1000) {
+                this.throw(errors.codes.rate_limit, "距离上次更改头像不足一天");
             }
 
-            await this.service.db.user.update_user(this.ctx.auth.user_id, {
-                avatar: {
-                    type: "upload",
-                    name: image_name,
-                    update_at: Date.now()
-                }
-            });
+            await this.service.cloud_storage.profile.delete_avatar(user_avatar.name);
         }
+
+        await this.service.db.user.update_user(this.ctx.auth.user_id, {
+            avatar: {
+                type: "upload",
+                name: image_name,
+                update_at: Date.now()
+            }
+        });
 
         return {
             upload_options
@@ -74,25 +72,23 @@ module.exports = class Service_User_Info extends Service {
         let upload_options = await this.service.cloud_storage.profile.get_profile_background_image_upload_options(image_name);
 
         let background_image = this.ctx.user.profile_background_image;
-        // 如果和目前user的profile_background_image不一致，则进行更新
-        if (!background_image || background_image.name !== image_name) {
-            if (background_image) {
-                let now_time = Date.now();
-                let diff = now_time - background_image.update_at;
-                if (diff < 24 * 60 * 60 * 1000) {
-                    this.throw(errors.codes.rate_limit, "距离上次更改背景图不足一天");
-                }
 
-                await this.service.cloud_storage.profile.delete_background_image(background_image.name);
+        if (background_image) {
+            let now_time = Date.now();
+            let diff = now_time - background_image.update_at;
+            if (diff < 24 * 60 * 60 * 1000) {
+                this.throw(errors.codes.rate_limit, "距离上次更改背景图不足一天");
             }
 
-            await this.service.db.user.update_user(this.ctx.auth.user_id, {
-                profile_background_image: {
-                    name: image_name,
-                    update_at: Date.now()
-                }
-            });
+            await this.service.cloud_storage.profile.delete_background_image(background_image.name);
         }
+
+        await this.service.db.user.update_user(this.ctx.auth.user_id, {
+            profile_background_image: {
+                name: image_name,
+                update_at: Date.now()
+            }
+        });
 
         return {
             upload_options

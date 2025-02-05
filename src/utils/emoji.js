1777b6539942c73615871454371fad5894ad8cc3
Acid-Lemon <3573045100@qq.com>
2025-02-06 01:21:50 +0800

    simple fix

---
+++ src/utils/emoji.js
@@ -0,0 +1,194 @@
+export const emoji = [
+    {
+        value: "**doge**",
+        path: "/static/emoji/doge.png",
+    },
+    {
+        value: "**OK**",
+        path: "/static/emoji/OK.png",
+    },
+    {
+        value: "**偷笑**",
+        path: "/static/emoji/偷笑.png",
+    },
+    {
+        value: "**傲娇**",
+        path: "/static/emoji/傲娇.png",
+    },
+    {
+        value: "**冷**",
+        path: "/static/emoji/冷.png",
+    },
+    {
+        value: "**吃瓜**",
+        path: "/static/emoji/吃瓜.png",
+    },
+    {
+        value: "**哈欠**",
+        path: "/static/emoji/哈欠.png",
+    },
+    {
+        value: "**哦呼**",
+        path: "/static/emoji/哦呼.png",
+    },
+    {
+        value: "**喜极而泣**",
+        path: "/static/emoji/喜极而泣.png",
+    },
+    {
+        value: "**喜欢**",
+        path: "/static/emoji/喜欢.png",
+    },
+    {
+        value: "**大哭**",
+        path: "/static/emoji/大哭.png",
+    },
+    {
+        value: "**大笑**",
+        path: "/static/emoji/大笑.png",
+    },
+    {
+        value: "**奋斗**",
+        path: "/static/emoji/奋斗.png",
+    },
+    {
+        value: "**奶茶干杯**",
+        path: "/static/emoji/奶茶干杯.png",
+    },
+    {
+        value: "**妙啊**",
+        path: "/static/emoji/妙啊.png",
+    },
+    {
+        value: "**委屈**",
+        path: "/static/emoji/委屈.png",
+    },
+    {
+        value: "**害羞**",
+        path: "/static/emoji/害羞.png",
+    },
+    {
+        value: "**尴尬**",
+        path: "/static/emoji/尴尬.png",
+    },
+    {
+        value: "**干杯**",
+        path: "/static/emoji/干杯.png",
+    },
+    {
+        value: "**微笑**",
+        path: "/static/emoji/微笑.png",
+    },
+    {
+        value: "**惊喜**",
+        path: "/static/emoji/惊喜.png",
+    },
+    {
+        value: "**惊讶**",
+        path: "/static/emoji/惊讶.png",
+    },
+    {
+        value: "**打call**",
+        path: "/static/emoji/打call.png",
+    },
+    {
+        value: "**抓狂**",
+        path: "/static/emoji/抓狂.png",
+    },
+    {
+        value: "**拥抱**",
+        path: "/static/emoji/拥抱.png",
+    },
+    {
+        value: "**捂脸**",
+        path: "/static/emoji/捂脸.png",
+    },
+    {
+        value: "**撇嘴**",
+        path: "/static/emoji/撇嘴.png",
+    },
+    {
+        value: "**支持**",
+        path: "/static/emoji/支持.png",
+    },
+    {
+        value: "**无语**",
+        path: "/static/emoji/无语.png",
+    },
+    {
+        value: "**星星眼**",
+        path: "/static/emoji/星星眼.png",
+    },
+    {
+        value: "**滑稽**",
+        path: "/static/emoji/滑稽.png",
+    },
+    {
+        value: "**热**",
+        path: "/static/emoji/热.png",
+    },
+    {
+        value: "**爱心**",
+        path: "/static/emoji/爱心.png",
+    },
+    {
+        value: "**生气**",
+        path: "/static/emoji/生气.png",
+    },
+    {
+        value: "**生病**",
+        path: "/static/emoji/生病.png",
+    },
+    {
+        value: "**疑惑**",
+        path: "/static/emoji/疑惑.png",
+    },
+    {
+        value: "**疼**",
+        path: "/static/emoji/疼.png",
+    },
+    {
+        value: "**笑哭**",
+        path: "/static/emoji/笑哭.png",
+    },
+    {
+        value: "**给心心**",
+        path: "/static/emoji/给心心.png",
+    },
+    {
+        value: "**胜利**",
+        path: "/static/emoji/胜利.png",
+    },
+    {
+        value: "**脱单doge**",
+        path: "/static/emoji/脱单doge.png",
+    },
+    {
+        value: "**调皮**",
+        path: "/static/emoji/调皮.png",
+    },
+    {
+        value: "**辣眼睛**",
+        path: "/static/emoji/辣眼睛.png",
+    },
+    {
+        value: "**酸了**",
+        path: "/static/emoji/酸了.png",
+    },
+    {
+        value: "**难过**",
+        path: "/static/emoji/难过.png",
+    },
+    {
+        value: "**雪花**",
+        path: "/static/emoji/雪花.png",
+    },
+    {
+        value: "**鸡腿**",
+        path: "/static/emoji/鸡腿.png",
+    },
+    {
+        value: "**鼓掌**",
+        path: "/static/emoji/鼓掌.png",
+    }
+]
--- src/views/comment/comment.vue
+++ src/views/comment/comment.vue
@@ -1,13 +1,13 @@
 <script>
-import {call_api} from "@/src/utils/cloud";
-import {ElNotification} from "element-plus";
-
-import {date_format} from "@/src/utils/time";
-
 import axios from "axios";
+import {ElNotification} from "element-plus";
 import {Message} from "@element-plus/icons-vue";
-
 import {use_user_info_store} from "../../stores/userInfo";
+
+import {call_api} from "@/src/utils/cloud";
+import {date_format} from "@/src/utils/time";
+import {get_avatar} from "@/src/utils/get_avatar";
+import {emoji} from "@/src/utils/emoji";
 
 export default {
     components: {Message},
@@ -22,206 +22,13 @@ export default {
             sentences: null,
             loading_more: false,
             has_more: true,
-            emoji_list: [
-                {
-                    value: "**doge**",
-                    path: "/static/emoji/doge.png",
-                },
-                {
-                    value: "**OK**",
-                    path: "/static/emoji/OK.png",
-                },
-                {
-                    value: "**偷笑**",
-                    path: "/static/emoji/偷笑.png",
-                },
-                {
-                    value: "**傲娇**",
-                    path: "/static/emoji/傲娇.png",
-                },
-                {
-                    value: "**冷**",
-                    path: "/static/emoji/冷.png",
-                },
-                {
-                    value: "**吃瓜**",
-                    path: "/static/emoji/吃瓜.png",
-                },
-                {
-                    value: "**哈欠**",
-                    path: "/static/emoji/哈欠.png",
-                },
-                {
-                    value: "**哦呼**",
-                    path: "/static/emoji/哦呼.png",
-                },
-                {
-                    value: "**喜极而泣**",
-                    path: "/static/emoji/喜极而泣.png",
-                },
-                {
-                    value: "**喜欢**",
-                    path: "/static/emoji/喜欢.png",
-                },
-                {
-                    value: "**大哭**",
-                    path: "/static/emoji/大哭.png",
-                },
-                {
-                    value: "**大笑**",
-                    path: "/static/emoji/大笑.png",
-                },
-                {
-                    value: "**奋斗**",
-                    path: "/static/emoji/奋斗.png",
-                },
-                {
-                    value: "**奶茶干杯**",
-                    path: "/static/emoji/奶茶干杯.png",
-                },
-                {
-                    value: "**妙啊**",
-                    path: "/static/emoji/妙啊.png",
-                },
-                {
-                    value: "**委屈**",
-                    path: "/static/emoji/委屈.png",
-                },
-                {
-                    value: "**害羞**",
-                    path: "/static/emoji/害羞.png",
-                },
-                {
-                    value: "**尴尬**",
-                    path: "/static/emoji/尴尬.png",
-                },
-                {
-                    value: "**干杯**",
-                    path: "/static/emoji/干杯.png",
-                },
-                {
-                    value: "**微笑**",
-                    path: "/static/emoji/微笑.png",
-                },
-                {
-                    value: "**惊喜**",
-                    path: "/static/emoji/惊喜.png",
-                },
-                {
-                    value: "**惊讶**",
-                    path: "/static/emoji/惊讶.png",
-                },
-                {
-                    value: "**打call**",
-                    path: "/static/emoji/打call.png",
-                },
-                {
-                    value: "**抓狂**",
-                    path: "/static/emoji/抓狂.png",
-                },
-                {
-                    value: "**拥抱**",
-                    path: "/static/emoji/拥抱.png",
-                },
-                {
-                    value: "**捂脸**",
-                    path: "/static/emoji/捂脸.png",
-                },
-                {
-                    value: "**撇嘴**",
-                    path: "/static/emoji/撇嘴.png",
-                },
-                {
-                    value: "**支持**",
-                    path: "/static/emoji/支持.png",
-                },
-                {
-                    value: "**无语**",
-                    path: "/static/emoji/无语.png",
-                },
-                {
-                    value: "**星星眼**",
-                    path: "/static/emoji/星星眼.png",
-                },
-                {
-                    value: "**滑稽**",
-                    path: "/static/emoji/滑稽.png",
-                },
-                {
-                    value: "**热**",
-                    path: "/static/emoji/热.png",
-                },
-                {
-                    value: "**爱心**",
-                    path: "/static/emoji/爱心.png",
-                },
-                {
-                    value: "**生气**",
-                    path: "/static/emoji/生气.png",
-                },
-                {
-                    value: "**生病**",
-                    path: "/static/emoji/生病.png",
-                },
-                {
-                    value: "**疑惑**",
-                    path: "/static/emoji/疑惑.png",
-                },
-                {
-                    value: "**疼**",
-                    path: "/static/emoji/疼.png",
-                },
-                {
-                    value: "**笑哭**",
-                    path: "/static/emoji/笑哭.png",
-                },
-                {
-                    value: "**给心心**",
-                    path: "/static/emoji/给心心.png",
-                },
-                {
-                    value: "**胜利**",
-                    path: "/static/emoji/胜利.png",
-                },
-                {
-                    value: "**脱单doge**",
-                    path: "/static/emoji/脱单doge.png",
-                },
-                {
-                    value: "**调皮**",
-                    path: "/static/emoji/调皮.png",
-                },
-                {
-                    value: "**辣眼睛**",
-                    path: "/static/emoji/辣眼睛.png",
-                },
-                {
-                    value: "**酸了**",
-                    path: "/static/emoji/酸了.png",
-                },
-                {
-                    value: "**难过**",
-                    path: "/static/emoji/难过.png",
-                },
-                {
-                    value: "**雪花**",
-                    path: "/static/emoji/雪花.png",
-                },
-                {
-                    value: "**鸡腿**",
-                    path: "/static/emoji/鸡腿.png",
-                },
-                {
-                    value: "**鼓掌**",
-                    path: "/static/emoji/鼓掌.png",
-                }
-            ],
+            emoji_list: emoji,
         };
     },
     async mounted() {
         await this.get_sentences();
         await this.get_message_list();
-        this.avatar = await this.get_avatar();
+        this.avatar = await get_avatar();
     },
     computed: {
         style_mode() {
@@ -271,7 +78,7 @@ export default {
             return await Promise.all(messages.map((message) => {
                 return new Promise(async (resolve) => {
                     message.create_at_format_str = date_format(new Date(message.create_at));
-                    message.user.avatar_url = await this.get_avatar(message.user?.avatar?.name, message.user?.avatar?.type, message.user?.avatar?.url);
+                    message.user.avatar_url = await get_avatar(message.user?.avatar?.name, message.user?.avatar?.type, message.user?.avatar?.url);
                     message.background_color = this.random_color();
                     resolve(message);
                 })

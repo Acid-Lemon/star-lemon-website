75f31b4b12c11c93839e5038f36e7bad29ab6034
Acid-Lemon <3573045100@qq.com>
2025-03-01 01:00:11 +0800

    feat: 添加网易云音乐播放器
    - 在页面底部左角添加了网易云音乐播放器的 iframe
    - 播放器设置为固定位置，高度为 52 像素，宽度为 298 像素
    - 播放器类型为 0，ID 为13381784307，自动播放开启，高度为 32 像素

--- src/app.vue
+++ src/app.vue
@@ -46,6 +46,8 @@ export default {
         <Copyright v-if="show?.hasOwnProperty('copyright') ? show.copyright: true"></Copyright>
         <AIAssistant v-if="user_info"/>
     </ElConfigProvider>
+    <iframe class="fixed left-1 bottom-1" height=52
+            src="//music.163.com/outchain/player?type=0&id=13381784307&auto=1&height=32" width=298></iframe>
 </template>
 
 <style>

92892e17ede378d427029d500f955ff6fc228cc3
Acid-Lemon <3573045100@qq.com>
2025-03-06 17:10:16 +0800

    feat(index): 添加雪景开关功能
    - 在页面右上角添加雪景开关按钮
    - 使用 Vue 的条件渲染功能控制雪景显示
    - 优化雪景组件，提高页面性能

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -12,7 +12,8 @@ export default {
                 "/static/picture/2.jpg"
             ],
             online_tools: [],
-            snowflakes: []
+            snow_switch: false,
+            snowflakes: [],
         };
     },
     mounted() {
@@ -106,20 +107,27 @@ export default {
                 </div>
             </div>
         </div>
+        <div class="fixed top-[20vh] right-[30px] w-[20px] h-[20px]">
+            <el-button class="w-full h-full" round @click="snow_switch = !snow_switch">
+                <svg fill="rgba(0,0,0,1)" height="20px" viewBox="0 0 24 24" width="20px"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M11.9998 3.29814L14.4451 1.66794L15.5545 3.33204L12.9998 5.03517V10.2678L17.5313 7.65149L17.7289 4.58748L19.7247 4.7162L19.5356 7.64899L22.17 8.95159L21.2836 10.7444L18.5313 9.38354L14.0001 11.9996L18.5317 14.616L21.284 13.2551L22.1704 15.0479L19.536 16.3505L19.7251 19.2833L17.7293 19.412L17.5317 16.348L12.9998 13.7315V18.9648L15.5545 20.6679L14.4451 22.332L11.9998 20.7018L9.55446 22.332L8.44506 20.6679L10.9998 18.9648V13.7319L6.46786 16.3484L6.27026 19.4124L4.2744 19.2836L4.46355 16.3508L1.8291 15.0483L2.71555 13.2554L5.46786 14.6163L10.0001 11.9996L5.46824 9.38319L2.71594 10.7441L1.82948 8.95124L4.46393 7.64864L4.27478 4.71585L6.27064 4.58713L6.46824 7.65113L10.9998 10.2674V5.03517L8.44506 3.33204L9.55446 1.66794L11.9998 3.29814Z"></path>
+                </svg>
+            </el-button>
+        </div>
+        <snowflake
+            v-for="(snowflake, index) in snowflakes"
+            v-if="snow_switch"
+            :key="index"
+            :duration="snowflake.duration"
+            :opacity="snowflake.opacity"
+            :size="snowflake.size"
+            :x="snowflake.x"
+            :y="snowflake.y"
+        />
     </div>
-    <snowflake
-        v-for="(snowflake, index) in snowflakes"
-        :key="index"
-        :duration="snowflake.duration"
-        :opacity="snowflake.opacity"
-        :size="snowflake.size"
-        :x="snowflake.x"
-        :y="snowflake.y"
-    />
 </template>
 
 <style scoped>
-.el-carousel {
-    height: 100%;
-}
 </style>

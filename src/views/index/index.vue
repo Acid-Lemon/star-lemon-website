27ce8ebe3b8e5085a7db41661d7d683a239a9989
Acid-Lemon <3573045100@qq.com>
2025-05-02 22:23:13 +0800

    feat(index): 添加文件快传功能并优化首页布局
    - 在实用功能卡片中添加了文件快传按钮，用户可快速跳转至文件快传工具页面
    -为适应更多内容，首页添加了滚动条，提高了页面的可用性和用户体验

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -57,71 +57,76 @@ export default {
 
 <template>
     <div class="bg-[url('/static/background/22.jpg')] bg-cover bg-center w-full h-full pt-[100px]">
-        <div class="flex flex-col">
-            <uni-notice-bar background-color="#FFFFFF" scrollable show-icon
-                            text="欢迎来到star和lemon的小站!~ 我们网站将不断迭代, 为大家提供更多功能, 让大家在这个互联网的时代, 体验到更多科技带来的便捷与乐趣!"/>
-            <div class="flex flex-row">
-                <div class="flex flex-row mt-[20px]">
-                    <div class="flex flex-col">
-                        <div class="sm:w-[500px] w-[90vw] sm:h-[280px] h-[50vw] sm:ml-[20px] ml-[5vw]">
-                            <el-carousel motion-blur trigger="click">
-                                <el-carousel-item v-for="picture in picture_url" :key="picture">
-                                    <el-image :src="picture" fit="cover"/>
-                                </el-carousel-item>
-                            </el-carousel>
-                        </div>
-                        <div class="sm:w-[500px] w-[90vw] ml-[20px] mt-[20px]">
-                            <el-card style="max-width: 500px">
-                                <template #header>
-                                    <div class="card-header">
-                                        <span>实用功能</span>
-                                    </div>
-                                </template>
-                                <el-button @click="this.$router.push('/useful_tools/separate_audio')">
-                                    视频音频分离
-                                </el-button>
-                            </el-card>
-                        </div>
-                        <div class="sm:w-[500px] w-[90vw] ml-[20px] mt-[20px]">
-                            <el-card style="max-width: 500px">
-                                <template #header>
-                                    <div class="card-header">
-                                        <span>在线小工具</span>
-                                        <span class="text-[#88888888]">（来源于外部网站）</span>
-                                    </div>
-                                </template>
-                                <el-button v-for="tool in online_tools" @click="open_url(tool.url)">
-                                    {{ tool.name }}
-                                </el-button>
-                            </el-card>
+        <el-scrollbar>
+            <div class="flex flex-col mb-[80px] md:mb-[40px]">
+                <uni-notice-bar background-color="#FFFFFF" scrollable show-icon
+                                text="欢迎来到star和lemon的小站!~ 我们网站将不断迭代, 为大家提供更多功能, 让大家在这个互联网的时代, 体验到更多科技带来的便捷与乐趣!"/>
+                <div class="flex flex-row">
+                    <div class="flex flex-row mt-[20px]">
+                        <div class="flex flex-col">
+                            <div class="sm:w-[500px] w-[90vw] sm:h-[280px] h-[50vw] sm:ml-[20px] ml-[5vw]">
+                                <el-carousel motion-blur trigger="click">
+                                    <el-carousel-item v-for="picture in picture_url" :key="picture">
+                                        <el-image :src="picture" fit="cover"/>
+                                    </el-carousel-item>
+                                </el-carousel>
+                            </div>
+                            <div class="sm:w-[500px] w-[90vw] ml-[20px] mt-[20px]">
+                                <el-card style="max-width: 500px">
+                                    <template #header>
+                                        <div class="card-header">
+                                            <span>实用功能</span>
+                                        </div>
+                                    </template>
+                                    <el-button @click="this.$router.push('/useful_tools/separate_audio')">
+                                        视频音频分离
+                                    </el-button>
+                                    <el-button @click="this.$router.push('/useful_tools/quick_file_transfer')">
+                                        文件快传
+                                    </el-button>
+                                </el-card>
+                            </div>
+                            <div class="sm:w-[500px] w-[90vw] ml-[20px] mt-[20px]">
+                                <el-card style="max-width: 500px">
+                                    <template #header>
+                                        <div class="card-header">
+                                            <span>在线小工具</span>
+                                            <span class="text-[#88888888]">（来源于外部网站）</span>
+                                        </div>
+                                    </template>
+                                    <el-button v-for="tool in online_tools" @click="open_url(tool.url)">
+                                        {{ tool.name }}
+                                    </el-button>
+                                </el-card>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="flex flex-row mt-[20px]">
-                    <div class="flex flex-col">
-                        <div class="md:w-[500px] w-[90vw] md:h-[280px] h-[50vw] md:ml-[20px] ml-[5vw]">
-                        </div>
-                        <div class="w-[500px] ml-[20px] mt-[20px]">
+                    <div class="flex flex-row mt-[20px]">
+                        <div class="flex flex-col">
+                            <div class="md:w-[500px] w-[90vw] md:h-[280px] h-[50vw] md:ml-[20px] ml-[5vw]">
+                            </div>
+                            <div class="w-[500px] ml-[20px] mt-[20px]">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="fixed top-[20vh] right-[30px] w-[20px] h-[20px]">
-            <el-button class="w-full h-full" round @click="snow_switch = !snow_switch">
-                <i class="ri-snowflake-line"></i>
-            </el-button>
-        </div>
-        <snowflake
-            v-for="(snowflake, index) in snowflakes"
-            v-if="snow_switch"
-            :key="index"
-            :duration="snowflake.duration"
-            :opacity="snowflake.opacity"
-            :size="snowflake.size"
-            :x="snowflake.x"
-            :y="snowflake.y"
-        />
+            <div class="fixed top-[20vh] right-[30px] w-[20px] h-[20px]">
+                <el-button class="w-full h-full" round @click="snow_switch = !snow_switch">
+                    <i class="ri-snowflake-line"></i>
+                </el-button>
+            </div>
+            <snowflake
+                v-for="(snowflake, index) in snowflakes"
+                v-if="snow_switch"
+                :key="index"
+                :duration="snowflake.duration"
+                :opacity="snowflake.opacity"
+                :size="snowflake.size"
+                :x="snowflake.x"
+                :y="snowflake.y"
+            />
+        </el-scrollbar>
     </div>
 </template>
 
